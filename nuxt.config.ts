9356a98e201a7d22545271e3e6a5046f31e68a3a
ahmedragab <ahmedragab20901@gmail.com>
2023-05-24 22:30:28 +0300

    👀 UPDATE DEPENDENCIES

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
 
     config: {
       apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-      authDomain: 'auth-79e70.firebaseapp.com',
+      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
       projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
       storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
       messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
