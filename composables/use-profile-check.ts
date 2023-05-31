b602b6d514e310202eb59d34abcb044f9aa12b84
ahmedragab <ahmedragab20901@gmail.com>
2023-05-31 20:59:55 +0300

    ✅ comments added

--- composables/use-profile-check.ts
+++ composables/use-profile-check.ts
@@ -3,6 +3,9 @@ export default function () {
 
   function profileCheck(fn: Function): void {
     //TODO:: Create global function that handles this case throughout the app
+    //? current issue: what if the user reacted without completing the profile?
+    //* -> make the update profile mode reusable and pop it up when the user reacts.
+
     if (!user) {
       return;
     }
