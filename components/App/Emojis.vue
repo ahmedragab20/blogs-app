fee9aee79016739a1601cdd60be9ddb7eb019d6f
ahmedragab <ahmedragab20901@gmail.com>
2023-05-31 16:22:47 +0300

    🏃🏻‍♂️ Code still going

--- components/App/Emojis.vue
+++ components/App/Emojis.vue
@@ -1,26 +1,29 @@
 <template>
-  <div @mouseleave="emojiPickerUp ? emojiPickerUpHandler(false) : () => {}" class="relative">
-    <UButton
-      :id="`picker-btn_${componentId}`"
-      class="relative z-20"
-      v-bind="btnBinds"
-      @click="emojiPickerUpHandler()"
-    >
+  <div class="relative">
+    <UButton id="picker-btn" v-bind="btnBinds" @click="toggle">
       <slot />
     </UButton>
 
     <div
-      @mouseleave="emojiPickerUpHandler(false)"
-      :id="`picker_${componentId}`"
-      class="absolute -top-[130%] -left-1/2"
+      id="picker"
+      class="absolute sm:-bottom-[50px] -bottom-20 sm:left-10 left-0 z-10 sm:scale-100 scale-75"
     >
       <Transition name="slide-fade">
+        <!-- <Picker
+          v-if="emojiPickerSelected"
+          :data="emojiIndex"
+          title="How do u c the blog?"
+          emoji="point_up"
+          class="shadow-xl shadow-primary-100 text-primary-500"
+          @select="updateEmoji"
+        /> -->
         <div
-          v-if="emojiPickerUp"
-          class="w-64 p-2 rounded-lg bg-white shadow-xl z-30"
-          @mouseover="emojiPickerUp = true"
+          v-if="emojiPickerSelected"
+          class="w-96 h-36 shadow-lg flex justify-center items-center shadow-purple-200 rounded-xl bg-white"
         >
-          hello gangiiii
+          <p v-twemoji>
+            {{ emojiText }}
+          </p>
         </div>
       </Transition>
     </div>
@@ -31,28 +34,37 @@
   const emit = defineEmits<{
     'update-emoji': [emoji: any];
   }>();
-
   const { btnBinds } = defineProps<{
     btnBinds: object;
   }>();
+  const nuxtApp = useNuxtApp();
 
-  const componentId = ref(Generics.uuid());
-  const emojiPickerUp = ref(false);
-  const emojiPickerUpHandler = (value?: boolean) => {
-    console.log('emojiPickerUpHandler', value);
+  const emojiText = ref('I ❤️ Twemoji!');
 
-    if (value === undefined) {
-      if (!emojiPickerUp.value) {
-        emojiPickerUp.value = true;
-      } else {
-        setTimeout(() => {
-          emojiPickerUp.value = false;
-        }, 1000);
-      }
-    } else {
-      setTimeout(() => {
-        emojiPickerUp.value = value as boolean;
-      }, 100);
-    }
+  // const emojiIndex = ref(nuxtApp.$emojiIndex);
+
+  const emojiPickerSelected = ref(false);
+  const toggle = () => {
+    emojiPickerSelected.value = !emojiPickerSelected.value;
   };
+  const updateEmoji = (emoji: any) => {
+    toggle();
+    console.log(emoji);
+    emit('update-emoji', emoji);
+  };
+  onMounted(() => {
+    //     const picker = document.getElementById('picker') as HTMLElement;
+    //     const pickerBtn = document.getElementById('picker-btn') as HTMLElement;
+    //
+    //     document.addEventListener('click', (e) => {
+    //       if (
+    //         picker &&
+    //         pickerBtn &&
+    //         !picker.contains(e.target as Node) &&
+    //         !pickerBtn.contains(e.target as Node)
+    //       ) {
+    //         emojiPickerSelected.value = false;
+    //       }
+    //     });
+  });
 </script>
